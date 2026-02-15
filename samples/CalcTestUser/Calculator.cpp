bf082a861adc15e57ab2ca504ec533b6b105f9d8
Sergey Podobry <sergey.podobry@gmail.com>
2026-02-15 23:12:38 +0200

    Add Linux usermode support and a sample for it

---
+++ samples/CalcTestUser/Calculator.cpp
@@ -0,0 +1,16 @@
+#include "Calculator.h"
+
+int Calculator::add(int x, int y)
+{
+    return x + y;
+}
+
+int Calculator::sub(int x, int y)
+{
+    return x - y - 1;
+}
+
+long long Calculator::mul(int x, int y)
+{
+    return x * y;
+}
